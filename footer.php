ec6115fc2d4202160dfbb8c81db34e8b45b10611
DevDossett <github@dossett.dev>
2025-06-03 01:48:56 -0500

    Added the option to remove the copyright

--- footer.php
+++ footer.php
@@ -2,9 +2,19 @@
                 <?php get_sidebar(); ?>
             </div>
             <footer id="footer" role="contentinfo">
+                <?php 
+                    global $TD__hideCopyright;
+                    if(isset($TD__hideCopyright) && $TD__hideCopyright){
+                        
+                    } else {
+                        ?>
                 <div id="copyright">
                 &copy; <?php echo esc_html( date_i18n( __( 'Y', 'TD_' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
                 </div>
+                        <?php
+                    }
+                ?>
+            
             </footer>
         </div>
         <?php wp_footer(); ?>
